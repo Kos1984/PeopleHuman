2edc714434e89934d4e626fe966d234c026aad36
Kos1984 <86412949+Kos1984@users.noreply.github.com>
2023-03-21 13:08:40 +0300

    main changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,15 @@
-fun main () {
+fun main() {
     val likes = 1001
 
     val message = "понравилось $likes человеку"
     val message1 = "понравилось $likes людям"
-    println(likes %10)
+    println(likes % 10)
 
 
-    if (likes %10 === 1 && likes != 11){
+    if (likes % 10 === 1 && likes != 11) {
         println(message)
-    }else {
+    } else {
         println(message1)
     }
-
 }
 
